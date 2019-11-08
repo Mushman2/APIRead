63a363b6d93641223d3c9b4c98c046b92171ad63
Mushman2 <hartigam@tcd.ie>
2019-11-08 11:13:28 +0000

    Added login and print more info

--- LowestCommonAncestor/src/Application.kt
+++ LowestCommonAncestor/src/Application.kt
@@ -1,17 +1,29 @@
 import org.eclipse.egit.github.core.client.GitHubClient
 import org.eclipse.egit.github.core.service.RepositoryService
+import org.eclipse.egit.github.core.service.UserService
 import java.util.*
 
 
 fun main(){
-    println("Please enter the username of the developer you wish to know about:")
-    val username = readLine()
+    val username = "Mushman2"
+    val loginToken = "bd20a7cd1937a0804f590dbbe9c270c8640f0451"
     val client = GitHubClient()
-    //client.setCredentials("Mushman2", "passw0rd");
-    val service = RepositoryService()
-    for (repo in service.getRepositories(username)) {
-        println("\n" + repo.name + " Watchers: " + repo.watchers + " Private?: " + repo.isPrivate + " \nBranches: ")
-        for (branch in service.getBranches(repo)) println("   " + branch.name)
+    client.setCredentials(username, loginToken)
+
+    val userService = UserService(client)
+    println("Emails: ")
+    for (email in userService.emails)
+        println("  $email")
+    println("Followers: ")
+    for (f in userService.followers)
+        println("  ${f.login}")
+
+    println("Repositories:")
+    val service = RepositoryService(client)
+    for (repo in service.repositories) {
+        println("  " + repo.name + ":\n    Watchers: " + repo.watchers + "\n    Private?: " + repo.isPrivate + " \n    Branches: ")
+        for (branch in service.getBranches(repo)) println("      " + branch.name)
     }
+
 
 }
