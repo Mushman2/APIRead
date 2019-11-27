d86847941844531e6a5dabe1b8d8f7aaba579f85
Mushman2 <hartigam@tcd.ie>
2019-11-27 01:16:30 +0000

    Dataset Aquired

--- APIRead/src/Application.kt
+++ APIRead/src/Application.kt
@@ -1,25 +1,43 @@
+import org.eclipse.egit.github.core.IRepositoryIdProvider
+import org.eclipse.egit.github.core.RepositoryCommit
 import org.eclipse.egit.github.core.client.GitHubClient
-            import org.eclipse.egit.github.core.service.RepositoryService
-            import org.eclipse.egit.github.core.service.UserService
+import org.eclipse.egit.github.core.service.CommitService
+import org.eclipse.egit.github.core.service.RepositoryService
+import org.eclipse.egit.github.core.service.UserService
+import java.io.File
 
-    fun main(){
+fun main(){
         val username = "Mushman2"
-        val loginToken = "bd20a7cd1937a0804f590dbbe9c270c8640f0451"
+        val loginToken = "326363b43560598c7d91ecf2e128a2b64123ae6a"
         val client = GitHubClient()
         client.setCredentials(username, loginToken)
-
+//
         val userService = UserService(client)
-        println("Emails: ")
-        for (email in userService.emails)
-            println("  $email")
-        println("Followers: ")
-        for (f in userService.followers)
-            println("  ${f.login}")
+//        println("Emails: ")
+//        for (email in userService.emails)
+//            println("  $email")
+//        println("Followers: ")
+//        for (f in userService.followers)
+//            println("  ${f.login}")
+//
+//        println("Repositories:")
+        val repositoryService = RepositoryService(client)
+        val commitService = CommitService(client)
+//        for (repo in service.repositories) {
+//            println("  " + repo.name + ":\n    Watchers: " + repo.watchers + "\n    Private?: " + repo.isPrivate + " \n    Branches: ")
+//            for (branch in service.getBranches(repo)) println("      " + branch.name)
+//        }
 
-        println("Repositories:")
-        val service = RepositoryService(client)
-        for (repo in service.repositories) {
-            println("  " + repo.name + ":\n    Watchers: " + repo.watchers + "\n    Private?: " + repo.isPrivate + " \n    Branches: ")
-            for (branch in service.getBranches(repo)) println("      " + branch.name)
+        val repo = repositoryService.getRepository("mbostock", "d3")
+
+        println(repo.createdAt)
+        val commitsList = commitService.getCommits({repo.generateId()})
+        val file = File("commits.csv")
+        for (commitSmall in commitsList){
+                val commit = commitService.getCommit({repo.generateId()},  commitSmall.sha)
+                if(commit.stats != null) {
+                        //println("${commit.commit.author.date}, ${commit.commit.author.name}, ${commit.commit.committer.name}, ${commit.stats.additions}, ${commit.stats.deletions}")
+                        file.appendText("${commit.commit.author.date}, ${commit.commit.author.name}, ${commit.commit.committer.name}, ${commit.stats.additions}, ${commit.stats.deletions}, ${commit.stats.total}, ${commit.commit.commentCount}\n")
+                }
         }
-}
+    }
