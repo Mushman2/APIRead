6b86436357728af2ebbfb26d91852e02c6bc7cd2
Mushman2 <hartigam@tcd.ie>
2019-11-28 16:51:09 +0000

    Fixed CSV Formatting

--- APIRead/src/Application.kt
+++ APIRead/src/Application.kt
@@ -1,43 +1,33 @@
-import org.eclipse.egit.github.core.IRepositoryIdProvider
-import org.eclipse.egit.github.core.RepositoryCommit
+
 import org.eclipse.egit.github.core.client.GitHubClient
 import org.eclipse.egit.github.core.service.CommitService
 import org.eclipse.egit.github.core.service.RepositoryService
 import org.eclipse.egit.github.core.service.UserService
 import java.io.File
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 fun main(){
         val username = "Mushman2"
         val loginToken = "326363b43560598c7d91ecf2e128a2b64123ae6a"
         val client = GitHubClient()
         client.setCredentials(username, loginToken)
-//
-        val userService = UserService(client)
-//        println("Emails: ")
-//        for (email in userService.emails)
-//            println("  $email")
-//        println("Followers: ")
-//        for (f in userService.followers)
-//            println("  ${f.login}")
-//
-//        println("Repositories:")
+
         val repositoryService = RepositoryService(client)
         val commitService = CommitService(client)
-//        for (repo in service.repositories) {
-//            println("  " + repo.name + ":\n    Watchers: " + repo.watchers + "\n    Private?: " + repo.isPrivate + " \n    Branches: ")
-//            for (branch in service.getBranches(repo)) println("      " + branch.name)
-//        }
-
         val repo = repositoryService.getRepository("mbostock", "d3")
 
         println(repo.createdAt)
         val commitsList = commitService.getCommits({repo.generateId()})
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")
         val file = File("commits.csv")
+        file.appendText("date,author,committer,additions,deletions,total,comments,messageLength\n")
+
         for (commitSmall in commitsList){
                 val commit = commitService.getCommit({repo.generateId()},  commitSmall.sha)
                 if(commit.stats != null) {
                         //println("${commit.commit.author.date}, ${commit.commit.author.name}, ${commit.commit.committer.name}, ${commit.stats.additions}, ${commit.stats.deletions}")
-                        file.appendText("${commit.commit.author.date}, ${commit.commit.author.name}, ${commit.commit.committer.name}, ${commit.stats.additions}, ${commit.stats.deletions}, ${commit.stats.total}, ${commit.commit.commentCount}, ${commit.commit.message.length}\n")
+                        file.appendText("${commit.commit.author.date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime().format(formatter)},\"${commit.commit.author.name}\",\"${commit.commit.committer.name}\",${commit.stats.additions},${commit.stats.deletions},${commit.stats.total},${commit.commit.commentCount},${commit.commit.message.length}\n")
                 }
         }
     }
